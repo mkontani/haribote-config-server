e4f6d63a4711c3aec6fe72662bdcf730563e0a23
mkontani <itoama@live.jp>
2020-04-04 14:59:50 +0900

    smallfix

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ let config
 if (process.argv.length === 3 && fs.existsSync(process.argv[2])) config = require(process.argv[2])
 else config = fs.existsSync(process.env.HARIBOTE_CONF) ? require(process.env.HARIBOTE_CONF) : require('./settings.json')
 
-console.log(process.argv)
-
 if (!config) {
     console.log('config file is not defined.')
     process.exit(1)
