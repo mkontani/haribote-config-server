0ce2f4316ac1fc5ed1dbe7a0e60ade7ccbad0615
mkontani <itoama@live.jp>
2020-09-21 11:49:58 +0900

    support request condition

--- server.js
+++ server.js
@@ -10,17 +10,13 @@ const Up = (config) => {
         key: fs.readFileSync(c.tls.key),
         cert: fs.readFileSync(c.tls.cert)
       }, (req, res) => {
-        res.statusCode = c.statusCode || 200
-        req.headers.haribote_name = c.name || 'haribote-server'
-        const resMessage = c.resMessage || JSON.stringify(req.headers, null, 4)
-        res.end(resMessage)
+        req.protocol = 'https'
+        apply(req, res, c)
       })
     } else {
       server = http.createServer((req, res) => {
-        res.statusCode = c.statusCode || 200
-        req.headers.haribote_name = c.name || 'haribote-server'
-        const resMessage = c.resMessage || JSON.stringify(req.headers, null, 4)
-        res.end(resMessage)
+        req.protocol = 'http'
+        apply(req, res, c)
       })
     }
     server.listen(c.port, () => {
@@ -33,6 +29,46 @@ const Up = (config) => {
       process.exit(1)
     })
   })
+}
+
+const apply = (req, res, c) => {
+  // default settings
+  req.headers.haribote_name = c.name || 'haribote-server'
+  const resBody = JSON.stringify(req.headers, null, 2)
+
+  const url = new URL(req.url, `${req.protocol}://${req.headers.host}`)
+
+  // undefined mapping
+  if (!c.mappings) {
+    res.statusCode = 200
+    res.end(resBody)
+    return
+  } else {
+    // check mappings
+    let resContentType, resStatusCode, resBody
+    c.mappings.forEach(m => {
+      // method and path matched
+      if ((m.req.path === url.pathname &&
+        m.req.method && m.req.method.toUpperCase() === req.method) ||
+        // path matched and method undefined
+        (!m.req.method && m.req.path === url.pathname) ||
+        // method matched and path undefined
+        (m.req.method && m.req.method.toUpperCase() === req.method && !m.req.path)) {
+        resContentType = m.res.contentType
+        resStatusCode = m.res.statusCode
+        resBody = m.res.body
+      }
+    })
+    if (resContentType || resStatusCode || resBody) {
+      res.setHeader('content-type', resContentType || 'plain/text')
+      res.statusCode = resStatusCode || 200
+      res.end(JSON.stringify(resBody) || resBody)
+      return
+    }
+  }
+  // has mapping, but undefined case
+  res.statusCode = 404
+  res.end(resBody)
 }
 
 module.exports = Up
