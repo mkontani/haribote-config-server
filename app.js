9eb2cd85a7c50d1da8143da7f7141521cfaf8edb
mkontani <itoama@live.jp>
2020-10-25 00:19:35 +0900

    add test

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const fs = require('fs')
 const path = require('path')
 const hariboteUp = require('./server')
 
+// help option
+if (process.argv.length === 3 && (process.argv[2] === '--help' || process.argv[2] === '-h')) {
+  console.log(`Usage:
+Specify configfile path by argv or env (HARIBOTE_CONF)
+ARGV Case:
+  e.g.) $ haribote-config-server ./settings.json
+ENV Case:
+  e.g.) $ HARIBOTE_CONF=./settings.json haribote-config-server
+  `)
+  process.exit(1)
+}
+
 // get config
 let cpath
 const inpath = (process.argv.length === 3) ? process.argv[2] : process.env.HARIBOTE_CONF
