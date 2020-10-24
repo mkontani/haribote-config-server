1caaca9c418422ec6144314f3d619de6b33b2ac8
mkontani <itoama@live.jp>
2020-10-25 00:23:01 +0900

    fix lint

--- test/server.spec.js
+++ test/server.spec.js
@@ -9,24 +9,24 @@ const isMethodMatch = server.__get__('isMethodMatch')
 describe('isPathMatch function', () => {
   it('path undefined case', () => {
     assert.strictEqual(isPathMatch(undefined, '/foo/bar'), false)
-  });
+  })
   it('wildcard unused case - path match', () => {
     assert.strictEqual(isPathMatch('/foo/bar', '/foo/bar'), true)
-  });
+  })
   it('wildcard unused case - path not match', () => {
     assert.strictEqual(isPathMatch('/foo/bar', '/foo/bar/baz'), false)
-  });
+  })
   it('deep wildcard used case - path match', () => {
     assert.strictEqual(isPathMatch('/foo/**', '/foo/bar/baz'), true)
     assert.strictEqual(isPathMatch('**/baz', '/foo/bar/baz'), true)
     assert.strictEqual(isPathMatch('/foo/**/baz', '/foo/bar1/bar2/baz'), true)
-  });
+  })
   it('deep wildcard used case - path not match', () => {
     assert.strictEqual(isPathMatch('/bar/**', '/foo/bar/baz'), false)
     assert.strictEqual(isPathMatch('**/foo', '/foo/bar/baz'), false)
     assert.strictEqual(isPathMatch('/foo/**/baz', '/foo/bar1/bar2/bazz'), false)
     assert.strictEqual(isPathMatch('/foo/**/baz', '/fooo/bar1/bar2/baz'), false)
-  });
+  })
   it('single wildcard used case - path match', () => {
     assert.strictEqual(isPathMatch('/foo/*', '/foo/bar'), true)
     assert.strictEqual(isPathMatch('/*/baz', '/bar/baz'), true)
@@ -36,27 +36,26 @@ describe('isPathMatch function', () => {
     assert.strictEqual(isPathMatch('/foo/b*r/baz', '/foo/bar/baz'), true)
     assert.strictEqual(isPathMatch('/foo/*ar/baz', '/foo/bar/baz'), true)
     assert.strictEqual(isPathMatch('/foo/*bar/baz', '/foo/bar/baz'), true)
-  });
+  })
   it('single wildcard used case - path not match', () => {
     assert.strictEqual(isPathMatch('/foo/*', '/foo/bar/baz'), false)
     assert.strictEqual(isPathMatch('/*/baz', '/foo/bar/baz'), false)
     assert.strictEqual(isPathMatch('/foo/*/baz', '/fooo/bar/baz'), false)
     assert.strictEqual(isPathMatch('/foo/ba*/baz', '/foo/bbar/baz'), false)
-
-  });
-});
+  })
+})
 
 describe('isMethodMatch function', () => {
   it('method undefined case', () => {
     assert.deepStrictEqual(isMethodMatch(undefined, 'GET'), false)
-  });
+  })
   it('wildcard case', () => {
     assert.deepStrictEqual(isMethodMatch('*', 'GET'), true)
-  });
+  })
   it('specify method case - request match', () => {
     assert.deepStrictEqual(isMethodMatch('get', 'GET'), true)
-  });
+  })
   it('specify method case - request not match', () => {
     assert.deepStrictEqual(isMethodMatch('get', 'POST'), false)
-  });
-});
+  })
+})
